6541a97ebf77863d6976066ae1633a8b9eeea344
朱鴻埕 <marc.zhu@ehsn.com.tw>
2023-07-18 18:23:55 +0800

    login register css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import GoogleButton from "react-google-button";
 
 const Login = () => {
   const nagivate = useNavigate();
@@ -31,12 +32,16 @@ const Login = () => {
       <div className="my-5 xl:mt-0 card flex flex-col  xl:grid xl:grid-cols-2 bg-white w-5/6 xl:w-1/2 min-h-[600px]  rounded-lg overflow-hidden">
         <div className="left col-span-1  bg-blue-500  bg-opacity-80 p-[50px] flex flex-col justify-center gap-[30px] text-white">
           <h1 className="text-[30px] text-center">貼文，留言，聊天</h1>
-          <h1 className="text-[50px] font-bold text-center">請先登入</h1>
+          <h1 className="text-[60px] font-bold text-center">請先登入</h1>
+          <span className="text-[24px] text-center">沒有帳號，請註冊</span>
+          <Link to="/register">
+            <button className="w-[50%] p-[10px] bg-purple-200 border-none font-bold cursor-pointer rounded-[10px] text-black">
+              Register
+            </button>
+          </Link>
         </div>
         <div className="right col-span-1 p-[50px] flex flex-col justify-center gap-[30px] ">
-          <div onClick={googleAuth}>google</div>
-
-          <div>github</div>
+          <GoogleButton label="Google 登入" onClick={googleAuth} />
           <h1 className="text-[60px] ">Login</h1>
           <form className="flex flex-col gap-[30px]">
             <input
@@ -57,7 +62,7 @@ const Login = () => {
             />
             <div
               onClick={normalLogin}
-              className="w-[50%] p-[10px] text-center bg-purple-200 font-bold curser-pointer "
+              className="w-[50%] p-[10px] text-center bg-blue-200 font-bold cursor-pointer rounded-[10px] "
             >
               login
             </div>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import GoogleButton from "react-google-button";
 
 const Register = () => {
   const nagivate = useNavigate();
   // 待改
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const googleAuth = () => {
+    window.open(`http://localhost:8080/auth/google/callback`, "_self");
+  };
   const normalRegister = async () => {
     try {
       let response = await axios.post("http://localhost:8080/auth/register", {
@@ -22,23 +27,21 @@ const Register = () => {
   };
 
   return (
-    <div className=" login min-h-screen bg-purple-300 flex justify-center items-center ">
-      <div className="my-5 xl:mt-0 card flex flex-col  xl:grid xl:grid-cols-2 bg-white w-5/6 xl:w-1/2 min-h-[600px]  rounded-lg overflow-hidden">
-        <div className="left col-span-1  bg-blue-500  bg-opacity-80 p-[50px] flex flex-col gap-[30px] text-white">
-          <h1 className="text-[100px] leading-[100px]">Hello World.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span className="text-[24px]">Don't you have an account?</span>
+    <div className=" login min-h-screen bg-blue-500 flex justify-center items-center ">
+      <div className="my-5 xl:mt-0 card flex flex-col  xl:grid xl:grid-cols-2 bg-white w-5/6 xl:w-5/6 min-h-[600px]  rounded-lg overflow-hidden">
+        <div className="left col-span-1   bg-purple-300  bg-opacity-80 p-[50px] flex flex-col justify-center gap-[30px] text-white">
+          <h1 className="text-[60px] font-bold text-center text-black">註冊</h1>
+          <span className="text-[24px] text-center text-black">
+            若有帳號，請登入
+          </span>
           <Link to="/login">
-            <button className="w-[50%] p-[10px] bg-purple-200 border-none font-bold curser-pointer">
+            <button className="w-[50%] p-[10px]  bg-blue-500 border-none font-bold cursor-pointer rounded-[10px] text-white">
               Login
             </button>
           </Link>
         </div>
         <div className="right col-span-1 p-[50px] flex flex-col justify-center gap-[30px] ">
+          <GoogleButton label="Google 登入" onClick={googleAuth} />
           <h1 className="text-[60px] ">Register</h1>
           <form className="flex flex-col gap-[30px]">
             <input
@@ -59,7 +62,7 @@ const Register = () => {
             />
             <div
               onClick={normalRegister}
-              className="w-[50%] p-[10px] bg-purple-200 font-bold curser-pointer "
+              className="w-[50%] p-[10px] text-center bg-purple-200  font-bold cursor-pointer rounded-[10px] "
             >
               Register
             </div>
